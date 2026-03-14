d834709d2110fbd341c02aac2b6f7d561e17f1df
pratiksha-14 <115628818+pratiksha-14@users.noreply.github.com>
2026-03-14 14:26:52 +0530

    Update and rename README.md to README.java

--- README.java
+++ README.java
@@ -1,5 +1,4 @@
 # java-test-0005-final-21180-pratiksha
-Final Project Assignment - This repository contains the complete final project code and documentation.
 public class NumberPattern {
     public static void main(String[] args) {
         int num = 1;
